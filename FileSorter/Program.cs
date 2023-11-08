a4912ff7927afdbd53ccf0b9c78cdaed7c7666db
SURYANSH <suryanshshivaprasad9212@gmail.com>
2023-11-08 21:10:40 +0530

    Added File Utilities Project

---
+++ FileSorter/Program.cs
@@ -0,0 +1,61 @@
+﻿public class Sorter
+{
+    public static void Main(string[] args)
+    {
+        string? rootPath = @"C:\Users\SURYANSH\Documents";
+        string? destDir = Path.Combine(rootPath, "Docus");
+        string? newPath = Path.Combine(destDir, "Text Files");
+
+        //string[] dirs = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+
+        var txtFiles = Directory.GetFiles(destDir, "*.txt", SearchOption.AllDirectories);
+        //var pdfFiles = Directory.GetFiles(rootPath, "*.pdf", SearchOption.AllDirectories);
+
+
+        foreach(var txtFile in txtFiles)
+        {
+            Console.WriteLine(Path.GetFileName(txtFile));
+        }
+        
+        bool directoryExists = Directory.Exists(destDir);
+
+        if (directoryExists == false)
+        {
+            Console.WriteLine("Directory doesnt exist for Text Files");
+            Directory.CreateDirectory(newPath);
+        }
+        else
+        {
+            foreach(var txtFile in txtFiles)
+            {
+                string dFilePath = Path.Combine(destDir, Path.GetFileName(txtFile));
+                File.Move(txtFile, dFilePath);
+                Console.WriteLine($"Moved {txtFile} to {dFilePath}");
+            }
+        }
+
+        foreach (string dir in txtFiles)
+        {
+            string dFilePath = Path.Combine(newPath, Path.GetFileName(dir));
+            File.Move(dir, dFilePath);
+            Console.WriteLine($"Moved {dir} to {dFilePath}");
+        }
+        Sorter.DeleteRoutine(newPath);
+    }
+    public static void DeleteRoutine(string root)
+    {
+        string[] txt = Directory.GetFiles(root, "*.txt", SearchOption.AllDirectories);
+
+        foreach(string txtFile in txt)
+        {
+            FileInfo f = new FileInfo(txtFile);
+            
+            if(f.Length > 10 * 1024 * 1024 ) 
+            {
+                File.Delete(txtFile);
+                Console.WriteLine($"Deleted {txtFile} {f.Length} bytes");
+            }
+            Console.WriteLine("Check you Recycle Bin for Deleted files");
+        }
+    }
+}
---
+++ TempFolderCleaner/Cleaner.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using Humanizer;
+
+public class TempClean
+{
+    public static void Main(string[] args)
+    {
+        string? tempPath1 = @"C:\Windows\Temp";
+        string? tempPath2 = @"C:\Windows\Prefetch";
+
+        string[] dirs = Directory.GetDirectories(tempPath1, "*", SearchOption.AllDirectories);
+        var files1 = Directory.GetFiles(tempPath2, "*.*", SearchOption.AllDirectories);
+        var files2 = Directory.GetFiles(tempPath1,"*.*",SearchOption.AllDirectories);
+        
+        foreach(var dir in dirs)
+        {
+            if(Directory.Exists(dir))
+            {
+                DirectoryInfo indo = new DirectoryInfo(dir);
+                Console.WriteLine($"{Path.GetDirectoryName(dir)} : {indo.CreationTime}");
+                indo.Delete(true);
+            }
+        }
+        Console.WriteLine("Cleaning files");
+
+        foreach( var file in files1)
+        {
+            var info = new FileInfo(file);
+            var humanizedSize = info.Length.Bytes().Humanize();
+            Console.WriteLine($"{Path.GetFileName(file)} : {humanizedSize} bytes");
+            info.Delete();
+        }
+
+        foreach (var file in files2)
+        {
+            var info = new FileInfo(file);
+             
+            Console.WriteLine($"{Path.GetFileName(file)} : {info.Length} bytes ");
+            try
+            {
+                info.Delete();
+            }
+            catch(System.IO.IOException ex)  
+            {
+                Console.WriteLine($"Exception has occured here are the details \n{ex}");
+            }
+        }
+        Console.WriteLine("All Temp files and folders are cleaned");
+
+    }
+}
