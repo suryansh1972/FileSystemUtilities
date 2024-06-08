37077c2998373df08b487edfb4a4af97c7350029
Suryansh <suryanshshivaprasad9212@gmail.com>
2024-06-09 01:10:47 +0530

    new file:   DockerFile

--- FileSorter/obj/Debug/net6.0/FileSorter.AssemblyInfo.cs
+++ FileSorter/obj/Debug/net6.0/FileSorter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileSorter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ecbd95764d3b65813af9de74de60ee348ff258a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileSorter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileSorter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TempFolderCleaner/obj/Debug/net6.0/TempFolderCleaner.AssemblyInfo.cs
+++ TempFolderCleaner/obj/Debug/net6.0/TempFolderCleaner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TempFolderCleaner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ecbd95764d3b65813af9de74de60ee348ff258a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TempFolderCleaner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TempFolderCleaner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
